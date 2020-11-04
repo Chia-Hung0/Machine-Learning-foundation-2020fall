6be2131210d9c8dc1d9eba53976dca60c3a89f2c
Chia-Hung0 <68656371+Chia-Hung0@users.noreply.github.com>
2020-11-04 16:34:30 +0800

    Add files via upload

---
+++ hw1/hw1_17.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+import random
+in_filename = sys.argv[1]
+fin = open(in_filename, 'r')
+data = np.loadtxt(fin)
+update = np.full((100, 1), 1)
+data = np.append(update, data, axis = 1)
+w = np.zeros((1, 11))
+def perceptron(w, data):
+    j = 0
+    correct = 0
+    while correct <500:
+        k = random.randint(0, 99)
+        x = data[k, :11]
+        y = data[k, 11]
+        check = np.dot(w, x)
+        correct = correct + 1
+        if(check * y <= 0):
+            w = w + y * x
+            j = j+1
+            correct = 0
+    return w[0,0]
+ 
+number = []
+
+for i in range(1000):
+    j = perceptron(w, data)
+    number.append(j)  
+number.sort()    
+print((number[500]+number[499])/2)
+
+
+fin.close()
